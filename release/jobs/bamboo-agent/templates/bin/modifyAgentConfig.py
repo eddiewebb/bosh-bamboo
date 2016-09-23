df981f7a3da24af8b846663ec1d6a6068f1df110
Eddie Webb <ollitech@gmail.com>
2016-09-23 15:59:18 -0400

    agents give themselves intelligent names

--- release/jobs/bamboo-agent/templates/bin/modifyAgentConfig.py
+++ release/jobs/bamboo-agent/templates/bin/modifyAgentConfig.py
@@ -7,6 +7,9 @@ specFile.close()
 
 configTree = xml.etree.ElementTree.parse('/var/vcap/data/bamboo-agent/bamboo-agent.cfg.xml')
 name = configTree.find("agentDefinition/name")
-name.text = "BOSH Agent: " + spec['id']
+name.text = "BOSH: " + spec['id']
+
+description = configTree.find("agentDefinition/description")
+description.text = "BOSH Agent running on " + spec['id']
 
 configTree.write('/var/vcap/data/bamboo-agent/bamboo-agent.cfg.xml')
