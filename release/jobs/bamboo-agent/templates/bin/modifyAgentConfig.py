d487d6a04b9c06e291da2915317e6318720f7c98
Eddie Webb <ollitech@gmail.com>
2016-09-23 20:14:46 -0400

    agents know their name

--- release/jobs/bamboo-agent/templates/bin/modifyAgentConfig.py
+++ release/jobs/bamboo-agent/templates/bin/modifyAgentConfig.py
@@ -1,15 +1,25 @@
 from json import load
 import xml.etree.ElementTree
+import os.path
 
 specFile = open('/var/vcap/bosh/spec.json')
 spec = load(specFile)
 specFile.close()
 
-configTree = xml.etree.ElementTree.parse('/var/vcap/data/bamboo-agent/bamboo-agent.cfg.xml')
-name = configTree.find("agentDefinition/name")
-name.text = "BOSH: " + spec['id']
+if not os.path.isfile('var/vcap/data/bamboo-agent/bamboo-agent.cfg.xml'):
+	root = xml.etree.ElementTree.Element("configuration")
+	buildDir = xml.etree.ElementTree.SubElement(root,"buildWorkingDirectory")
+	buildDir.text = "/var/vcap/data/bamboo-agent/xml-data/build-dir"
+	definition = xml.etree.ElementTree.SubElement(root,"agentDefinition")
+	name = xml.etree.ElementTree.SubElement(definition,"name")
+	description = xml.etree.ElementTree.SubElement(definition,"description")
+	configTree = xml.etree.ElementTree.ElementTree(root)
+else:
+	configTree = xml.etree.ElementTree.parse('/var/vcap/data/bamboo-agent/bamboo-agent.cfg.xml')
+	name = definition.find("agentDefinition/name")
+	description = definition.find("agentDefinition/description")
 
-description = configTree.find("agentDefinition/description")
+name.text = "BOSH: " + spec['id']
 description.text = "BOSH Agent running on " + spec['id']
 
 configTree.write('/var/vcap/data/bamboo-agent/bamboo-agent.cfg.xml')
