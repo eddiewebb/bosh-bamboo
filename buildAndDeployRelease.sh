0b6d8f7102510e8d24fa866144887cc1fb71388b
EDWARD WEBB <EDWARD.WEBB@LibertyMutual.com>
2018-01-19 16:12:33 -0500

    updated to support cli v2 and new bosh-lite suggested config. INcludes latest JDK8 and bamboo releases

--- buildAndDeployRelease.sh
+++ buildAndDeployRelease.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+
+# Setup of a new/empty director from fresh bosh-lite install procedure
+# Only run these once, or customize to meet existing bosh directory Setup#use vbox config
+
+# Cloud config specifies network, vm sizes, etc.
+bosh -e vbox update-cloud-config cloud-config-vbox.yml
+#provide stemcell,get latest URL for "warden bosh-lite" from bosh.io
+#bosh -e vbox us https://s3.amazonaws.com/bosh-core-stemcells/warden/bosh-stemcell-3468.19-warden-boshlite-ubuntu-trusty-go_agent.tgz
+
+
+
+# you need to download jdk8 and bamboo from vendor, assumes they are in ~/Downloads
+# These commands must be run after any changes to the release files
 pushd release
-bosh create release --force
-bosh upload release
-bosh deploy --recreate --skip-drain
+mkdir -p blobs/bamboo
+mkdir -p blobs/jdk8
+bosh add-blob ~/Downloads/atlassian-bamboo-6.2.9.tar.gz bamboo/atlassian-bamboo-6.2.9.tar.gz
+bosh add-blob ~/Downloads/jdk-8u162-linux-x64.tar.gz jdk8/jdk-8u162-linux-x64.tar.gz
+bosh create-release --force
+bosh -e vbox upload-release
 popd
+
+
+# There is now a release created and uploaded, below is actual deployment of release
+
+
+#deploy!
+bosh -e vbox -d bamboo deploy manifest.yml
